18a12a50d18ca108e5b35021ce1daaa87c68bb85
bendehibahamcherif-oss <bendehiba.hamcherif@gmail.com>
2026-05-23 17:17:17 +0400

    Connect realtime bindings to regime engine

--- src/core/realtimeBindings.js
+++ src/core/realtimeBindings.js
@@ -1,10 +1,13 @@
 import { eventBus, EVENTS } from './eventBus';
 import { useMarketStore } from '../store/marketStore';
+import { updateRegime } from './regimeBindings';
 
 eventBus.on(EVENTS.PRICE_UPDATE, (payload) => {
   useMarketStore
     .getState()
-    .updatePrice(payload.symbol, payload.price);
+    .updatePrice(payload.symbol, payload);
+
+  updateRegime();
 });
 
 eventBus.on(EVENTS.SIGNAL_UPDATE, (payload) => {
