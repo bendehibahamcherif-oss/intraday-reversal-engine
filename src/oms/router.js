ce3a830af90c168eab60e71c8a2c2a743741a9ff
bendehibahamcherif-oss <bendehiba.hamcherif@gmail.com>
2026-05-23 17:48:20 +0400

    Add IBKR paper trading routing support

--- src/oms/router.js
+++ src/oms/router.js
@@ -1,16 +1,45 @@
 import { SimulatedBroker } from '../brokers/SimulatedBroker';
+import { IBKRPaperBroker } from '../brokers/IBKRPaperBroker';
 
 class OMSRouter {
   constructor() {
-    this.broker = new SimulatedBroker();
+    this.simulatedBroker = new SimulatedBroker();
+
+    this.ibkrPaperBroker = new IBKRPaperBroker({
+      gatewayUrl:
+        import.meta.env.VITE_IBKR_GATEWAY || '',
+      accountId:
+        import.meta.env.VITE_IBKR_ACCOUNT || '',
+    });
+
+    this.activeBroker = 'SIMULATED';
+  }
+
+  setBroker(name) {
+    this.activeBroker = name;
+  }
+
+  getBroker() {
+    switch (this.activeBroker) {
+      case 'IBKR_PAPER':
+        return this.ibkrPaperBroker;
+
+      case 'SIMULATED':
+      default:
+        return this.simulatedBroker;
+    }
   }
 
   async route(order) {
-    return this.broker.submitOrder(order);
+    const broker = this.getBroker();
+
+    return broker.submitOrder(order);
   }
 
   async cancel(orderId) {
-    return this.broker.cancelOrder(orderId);
+    const broker = this.getBroker();
+
+    return broker.cancelOrder(orderId);
   }
 }
 
